758a697fa79267cf19146da2a0c71555088bc0c7
uydev <lequocuyit@gmail.com>
2025-05-07 16:23:59 +0700

    chore: update configuration and clean up project structure
    - Changed server host from "192.168.100.126" to "192.168.100.20" and updated port to 3001 in lynx.config.ts.
    - Removed 'dist/' from .gitignore to allow tracking of distribution files.
    - Added new binary files: main.lynx.bundle and logo.fa2b23b5.png in the dist directory.

--- lynx.config.ts
+++ lynx.config.ts
@@ -4,8 +4,8 @@ import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
 
 export default defineConfig({
   server: {
-    host: "192.168.100.126",
-    port: 3000,
+    host: "192.168.100.20",
+    port: 3001,
   },
   plugins: [
     pluginQRCode({
